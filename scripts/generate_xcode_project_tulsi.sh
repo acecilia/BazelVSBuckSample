0fb903d27370f83ca10d910cdd95b58e9b919a64
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2019-10-11 00:33:46 +0200

    Changed the "generate_xcode_project_tulsi.sh" to use the "--create-tulsiproj" option provided by tulsi

--- scripts/generate_xcode_project_tulsi.sh
+++ scripts/generate_xcode_project_tulsi.sh
@@ -1,25 +1,54 @@
 set -eou pipefail
 
 ##############################################################
+# Constants
+##############################################################
+
+tulsi="/Applications/Tulsi.app/Contents/MacOS/Tulsi"
+output_path="config/bazel_config"
+tulsiproj_path="$output_path/BazelVSBuckSample.tulsiproj"
+tulsiconf_path="$tulsiproj_path/project.tulsiconf"
+tulsigen_path="$tulsiproj_path/Configs/BazelVSBuckSample.tulsigen"
+
+##############################################################
 # Get all targets in the repository that need to be added to the Xcode project 
 ##############################################################
 
-# Get targets list + add quotes and comas at start and end of line
-targets_list=$(make list BUILDTOOL=bazel | sed 's/^/"/; s/$/",/')
-# Remove spaces + Add new lines + add indentation + escape backslashes
-targets_list=$(echo $targets_list | sed 's/ //g' | sed 's/,/,\\n/g' | sed 's/"\/\//\\ \\ \\ \\ "\/\//g' | sed 's/\//\\\//g')
+# Get targets list + add target flags
+targets_list=$(make list BUILDTOOL=bazel | sed 's/^/--target /;')
 
 ##############################################################
-# Add all targets to the tulsiproj files
+# Generate tulsiproj
+# See: https://github.com/bazelbuild/tulsi/issues/104
 ##############################################################
 
-tulsiproj_path='config/bazel_config/BazelVSBuckSample.tulsiproj'
-regex_to_replace='(\"buildTargets\" : \[).*?(\])'
-# Replace the targets in the tulsigen file with the new targets
-perl -0pi -e "s/$regex_to_replace/\1\n$targets_list  \2/gs" "$tulsiproj_path/Configs/All.tulsigen"
+"$tulsi" -- \
+--create-tulsiproj "BazelVSBuckSample" \
+--bazel "/usr/local/bin/bazel" \
+--outputfolder "config/bazel_config" $targets_list
 
 ##############################################################
-# Generate Xcode project using the newly generated tulsigen
+# Patch generated tulsiproj
 ##############################################################
 
-/Applications/Tulsi.app/Contents/MacOS/Tulsi -- --bazel /usr/local/bin/bazel --genconfig "$tulsiproj_path:All"
+# Remove the 'additionalFilePaths'
+regex_to_replace='\  "additionalFilePaths\".*?\],\n'
+perl -0pi -e "s/$regex_to_replace//gs" "$tulsigen_path"
+
+# Fix the 'sourceFilters': using './...' does not include the 'Sources' directories
+# in the Xcode project ( no idea why). Using '//Libraries/...' fixes the problem
+perl -pi -e 's|\Q./...|//Libraries/...|g' "$tulsigen_path"
+
+# Remove the 'packages' from the tulsiconf file because every time they are
+# generated the sorting is different and messes up with version control.
+# Seems like removing them does not have any negative effect
+regex_to_replace='\  "packages\".*?\],\n'
+perl -0pi -e "s/$regex_to_replace//gs" "$tulsiconf_path"
+
+##############################################################
+# Generate Xcode project using the newly generated tulsiproj
+##############################################################
+
+"$tulsi" -- \
+--genconfig "$tulsiproj_path"
+
